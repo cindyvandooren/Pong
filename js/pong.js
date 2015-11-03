955e2ecfb5e66006449cce97befc9b1537b1f1f4
Cindy Van Dooren <cindy.vandooren@gmail.com>
2015-11-02 21:27:56 -0800

    Worked some more on game logic

--- js/pong.js
+++ js/pong.js
@@ -3,34 +3,41 @@
     window.Pong = {};
   }
 
+  // Make a config class for custom configuration.
+
   var Game = Pong.Game = function (context, width, height) {
   	this.context = context;
   	this.width = width;
   	this.height = height;
   	this.board = new Game.Board(this.context, this.width, this.height);
-  	// this.leftPaddle = new Game.Paddle(this.context, this.board);
-  	// this.rightPaddle = new Game.Paddle(this.context, this.board);
+  	this.leftPaddle = new Game.Paddle(this.context, this.board, true);
+  	this.rightPaddle = new Game.Paddle(this.context, this.board, false);
   	this.ball = new Game.Ball(this.context, this.board);
+  	this.score = [0, 0];
   };
 
   Game.prototype.step = function () {
-  	this.board.render();
-  	// this.leftPaddle.render();
-  	// this.rightPaddle.render();
-  	this.ball.render();
-  }
+  	// Change the positions of the ball based on velocity and directions
+  	// Paddles positions will be changed using user input
+  	// Make sure that the game will be rendered every interval step.
+  	// 
 
-  // Make a config class for custom configuration.
-  // Think about how to pass less things from game to board to ball.
-  // Possible to let the ball not be a part of the board?
+  };
 
-  // Game needs a step method that should render the board and the movement of the ball every time
+  Game.prototype.render = function () {
+  		this.board.render();
+  		this.leftPaddle.render();
+  		this.rightPaddle.render();
+  		this.ball.render();	
+  };
 
-  // Game needs a ball and two paddles
-  // Make a ball, board and a paddle class
-  // Make a game over method
+  Game.prototype.start = function () {
+  	this.pongInterval = window.setInterval(this.step.bind(this), 150);
+  };
 
-
+  Game.prototype.isNotOver = function () {
+  	return this.score[0] < 10 && this.score[1] < 10;
+  };
 
   var Board = Game.Board = function (context, width, height) {
   	this.context = context;
@@ -47,9 +54,9 @@
   	this.context.rect(0, 0, this.width, this.height);	
   	this.context.fillStyle = "#078C22";
   	this.context.fill();
-  	this.context.stroke();
 
   	// Middle line
+  	this.context.beginPath();
   	this.context.strokeStyle = "#ffffff";
   	this.context.lineWidth = 5;
   	this.context.moveTo(middleX, 0);
@@ -77,15 +84,28 @@
   	this.context.fill();
   };
 
-  var Paddle = Game.Paddle = function (context, board) {
+  var Paddle = Game.Paddle = function (context, board, leftOrRight) {
   	this.context = context;
   	this.board = board;
+  	this.left = leftOrRight;
+  	this.posY = this.board.height / 2;
   };
 
-  // Make sure to draw two paddles. Different approach when left and right paddle.
+  // Make sure to draw two paddles.
+  // Whether the paddle is drawn left or right, depends on the posX.
   Paddle.prototype.render = function () {
-  	this.context.strokeStyle = "#f2f20c";
-  	this.context.lineWidth = 10;
+  	var posX = this.left ? 0 : this.board.width - 13;
+  	this.context.beginPath();
+  	this.context.rect(posX, this.posY, 13, 40);
+  	this.context.fillStyle = "#f2f20c";
+ 		this.context.fill();
+  };
+
+  Paddle.prototype.move = function () {
+
+  };
+
+  Paddle.prototype.isHit = function () {
 
   };
 
@@ -101,6 +121,14 @@
   	this.context.arc(this.posX, this.posY, 20, 0, 2 * Math.PI, false);
   	this.context.fillStyle = "#f2f20c";
   	this.context.fill();
+  };
+
+  Ball.prototype.move = function () {
+
+  };
+
+  Ball.prototype.randomDirection = function () {
+
   };
 
 }) ();
